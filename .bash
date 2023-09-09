5e819c489fcf1256266881cff521c6e2efff07ea
Chiano <bigchiano@protonmail.com>
2023-09-10 02:20:41 +0300

    Create .bash

---
+++ .bash
@@ -0,0 +1,51 @@
+
+gp() {
+    git pull
+    git push
+}
+
+gac() {
+    if [ $# -eq 0 ]; then
+        echo "Please provide a commit message."
+    else
+        local commit_message="$1"
+        shift
+        while [ $# -gt 0 ]; do
+            commit_message="$commit_message $1"
+            shift
+        done
+        git add .
+        git commit -m "$commit_message"
+    fi
+}
+
+gacp() {
+    if [ $# -eq 0 ]; then
+        echo "Please provide a commit message."
+    else
+        local commit_message="$1"
+        shift
+        while [ $# -gt 0 ]; do
+            commit_message="$commit_message $1"
+            shift
+        done
+        git add .
+        git commit -m "$commit_message"
+        git pull
+        git push
+    fi
+}
+
+ginit() {
+    local remote_url="$1"
+    if [ -z "$remote_url" ]; then
+        echo "Please provide git remote url."
+    else
+        git init
+        git add .
+        git commit -m "Initial commit"
+        git remote add origin "$remote_url"
+        git branch -M main
+        git push -u origin main
+    fi
+}
